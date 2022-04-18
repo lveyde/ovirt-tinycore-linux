25daf534671b57ed30da52aa84f5791c8c0b59fd
Lev Veyde <lveyde@redhat.com>
2022-04-18 09:24:35 +0300

    Added sources for building ovirt-acpid package
    Signed-off-by: Lev Veyde <lveyde@redhat.com>

---
+++ pkgs/ovirt-acpid/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+PKGDIR=$(pwd)
+if [ ! -d builddir ]; then
+    mkdir builddir
+else
+    rm -rf builddir
+    mkdir builddir
+fi
+
+pushd builddir
+
+wget http://www.tinycorelinux.net/13.x/x86/tcz/acpid.tcz
+
+cp -v "${PKGDIR}/src/acpid.tcz.md5.txt" .
+md5sum -c acpid.tcz.md5.txt
+
+unsquashfs acpid.tcz
+cp -v "${PKGDIR}/src/acpid" squashfs-root/usr/local/tce.installed/acpid
+mksquashfs squashfs-root ovirt-acpid.tcz -b 4k -no-xattrs
+md5sum ovirt-acpid.tcz > ovirt-acpid.tcz.md5.txt
+
+cp -av ovirt-acpid.tcz* ../
+
+popd
