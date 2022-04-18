c8de5688852b54ff3d8adfe50ce94859ee82a95a
Lev Veyde <lveyde@redhat.com>
2022-04-18 17:34:44 +0300

    Renamed ovirt-acpid installation script
    Signed-off-by: Lev Veyde <lveyde@redhat.com>

--- pkgs/ovirt-acpid/build.sh
+++ pkgs/ovirt-acpid/build.sh
@@ -17,7 +17,7 @@ cp -v "${PKGDIR}/src/acpid.tcz.md5.txt" .
 md5sum -c acpid.tcz.md5.txt
 
 unsquashfs acpid.tcz
-cp -v "${PKGDIR}/src/acpid" squashfs-root/usr/local/tce.installed/acpid
+cp -v "${PKGDIR}/src/ovirt-acpid" squashfs-root/usr/local/tce.installed/ovirt-acpid
 mksquashfs squashfs-root ovirt-acpid.tcz -b 4k -no-xattrs
 md5sum ovirt-acpid.tcz > ovirt-acpid.tcz.md5.txt
 
